53cc3f26236d2ce7fe75aa4a0aff1f71c88bfb2d
L3vi <levistum@gmail.com>
2020-04-23 08:39:10 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: null,
+      isLoaded: false,
+      breeds: [],
+      breedPictureUrls: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://dog.ceo/api/breeds/list/all")
+      .then(res => {
+        console.log(res);
+        const breedList = Object.keys(res.data.message);
+        this.setState({ isLoaded: true, breeds: breedList });
+      })
+      .catch(err => this.setState({ error: err, isLoaded: true }));
+  }
+
+  render() {
+    const { error, isLoaded, breeds } = this.state;
+    if (error) {
+      return <div>Error: error.message</div>;
+    } else if (!isLoaded) {
+      return <div>Loading. . .</div>;
+    } else {
+      const navBar = (<div className="navBar">{breeds.map(breed => {
+        return (<button className="navButton" onClick={() => { this.getPictures(breed) }}>{breed}</button>);
+      })}</div>);
+
+      if (this.state.breedPictureUrls) {
+        return <div className="page">
+          <div>{navBar}</div>
+          <div>{this.state.breedPictureUrls.map(breedPictureUrl => {
+            return (<img src={breedPictureUrl} />);
+          })}
+          </div>
+        </div>;
+      } else {
+        return navBar;
+      }
+
+    }
+  }
+
+  getPictures(breed) {
+    axios.get(`https://dog.ceo/api/breed/${breed}/images/random/3`)
+      .then(res => {
+        console.log(res);
+        this.setState({ breedPictureUrls: res.data.message });
+      })
+      .catch(err => this.setState({ error: err, isLoaded: true }));
+  }
+}
+
+class SideBar extends React.Component {
+
+}
+
+class MainView extends React.Component {
+
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Page />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
